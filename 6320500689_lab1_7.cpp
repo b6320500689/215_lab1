7204d6538d785d09e117f31926d7942409e0d4be
b6320500689 <supitchaya.r@ku.th>
2021-02-04 18:02:16 +0700

    time

--- 6320500689_lab1_7.cpp
+++ 6320500689_lab1_7.cpp
@@ -1,10 +1,24 @@
 #include <stdio.h>
 int main ()
 {
-	flost n=0;
-	scanf ("%f",&f);
-	if ()
-	{
-		printf ("%f a.m.",);
-	}
+    float time;
+    scanf ("%f",&time);
+    int t,ti,s,se;
+    t=(time*100)/100;
+    ceil (t) ;
+    s=(time*100);
+    se=s%100;
+    if (t<12)
+    {
+        printf ("%d:%d a.m.",t,se);
+    }
+    else
+    {
+        ti=t-12;
+        if(se==0)
+        printf ("%d:%d%d p.m.",ti,se,se);
+        else
+        printf ("%d:%d p.m.",ti,se);
+    }
+    return 0;
 }
