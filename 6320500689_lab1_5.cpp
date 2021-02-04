3a601269c52485526d77b428487e0f245a4803d2
b6320500689 <supitchaya.r@ku.th>
2021-02-04 17:27:17 +0700

    add j

--- 6320500689_lab1_5.cpp
+++ 6320500689_lab1_5.cpp
@@ -1,17 +1,23 @@
 #include <stdio.h>
 int main()
 {
-    int num,i,max=0;
+    int num,i,j,max=0;
     scanf("%d",&num);
     int a[num];
     for(i=0; i<num; i++)
     {
         scanf("%d",&a[i]);
-        if(a[i]>=max)
+    }
+    max=a[0];
+    for (i=1;i<num;i++)
+    {
+    	if(a[i]>=max)
         {
             max=a[i];
+            j=i;
         }
-    }
-    printf("%d %d",a[i],max);
+	}
+        
+    printf("%d %d",j+1,max);
     return 0;
 }
